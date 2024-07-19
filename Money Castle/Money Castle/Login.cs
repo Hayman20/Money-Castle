4ecef1667be06cca4f635b7adf5d0e97b5f91c49
Hayman20 <106497050+Hayman20@users.noreply.github.com>
2024-07-19 13:30:00 +1000

    zsf

--- Money Castle/Money Castle/Login.cs
+++ Money Castle/Money Castle/Login.cs
@@ -36,6 +36,14 @@ namespace Money_Castle
         private void pictureBox2_Click(object sender, EventArgs e)
         {
         }
+        void open() 
+        {
+            Page form = new Page();
+            form.Show();
+            this.Hide();
+            form.Closed += (s, args) => this.Close();
+
+        }
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
